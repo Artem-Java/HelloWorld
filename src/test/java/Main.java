934efe29b58d7624add128e4a18db8c03e410a97
TemaWS <tema409@mail.ru>
2023-05-23 21:13:48 +0300

    1.1

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("My first git repository");
-        System.out.println("Hello world!");
-        System.out.println("Hi all");
+        System.out.println("Hello world");
 
     }
 
